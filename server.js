ea96fff79f8c2f3bb94dedde440f2f33c7306f08
Dillon Grove <dillongrove@gmail.com>
2014-02-26 21:13:42 -0500

    Organized server.js and added home route and some additional configuration

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
+/* ---------- IMPORTS ------------------------------------------------------- */
+
+var path = require("path");
 var express = require("express");
 var app = express();
 
-app.use("/static", express.static(__dirname + "/static"));
+/* ---------- CONFIGURATION ------------------------------------------------- */
 
-app.listen(8000);
+app.configure(function(){ 
+  app.use(express.bodyParser());
+  app.use(express.static(path.join(__dirname, '/static')));
+  app.set("port", process.env.PORT || 8000);
+});
+
+/* ---------- ROUTES -------------------------------------------------------- */
+
+// Home Route
+app.get('/', function(req, res) {
+  res.sendfile('static/index.html');
+});
+
+/* ---------- RUN SERVER ---------------------------------------------------- */
+
+app.listen(app.get("port"));
+console.log("Listening on " + app.get("port"));
