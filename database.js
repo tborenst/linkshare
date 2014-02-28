d2a448799a8c8848cd80da353338b62af9e2b9bc
Tomer <tomer.boren@gmail.com>
2014-02-27 21:47:51 -0500

    converted tabs to spaces

--- database.js
+++ database.js
@@ -12,9 +12,9 @@ var options = {w: 1}; // enable write access
 var dbName = "linkshareDb";
 
 var client = new mongo.Db(
-	dbName, 
-	new mongo.Server(host, port),
-	options
+    dbName,
+    new mongo.Server(host, port),
+    options
 );
 var dbOpen = false;
 
@@ -29,23 +29,23 @@ openDb();
  * - make sure to call closeDb() at the end of your callback
  ***/
 var openDb = function(callback){
-	callback = (callback === undefined) ? function(){} : callback;
-	client.open(function(error){
-		if(error){
-			callback(error);
-		} else {
-			dbOpen = true;
-			callback(null);
-		}
-	});
+    callback = (callback === undefined) ? function(){} : callback;
+    client.open(function(error){
+        if(error){
+            callback(error);
+        } else {
+            dbOpen = true;
+            callback(null);
+        }
+    });
 }
 
 var closeDb = function(callback){
-	callback = (callback === undefined) ? function(){} : callback;
-	client.close(function(){
-		dbOpen = false;
-		callback();
-	});
+    callback = (callback === undefined) ? function(){} : callback;
+    client.close(function(){
+        dbOpen = false;
+        callback();
+    });
 }
 
 /**
@@ -55,25 +55,25 @@ var closeDb = function(callback){
  * - veto is an optional parameter - set to true, this function will do nothing
  ***/
 var getCollection = function(name, callback, veto){
-	// function to open a collection
-	var openCollection = function(){
-		client.collection(name, function(error, collection){
-			if(error){
-				callback(error, null);
-			} else {
-				callback(null, collection);
-			}
-		});
-	}
+    // function to open a collection
+    var openCollection = function(){
+        client.collection(name, function(error, collection){
+            if(error){
+                callback(error, null);
+            } else {
+                callback(null, collection);
+            }
+        });
+    }
 
-	// wait until the database is open before you open the collection
-	if(dbOpen){
-		openCollection();
-	} else {
-		setTimeout(function(){
-			getCollection(name, callback);
-		}, 0);
-	}
+    // wait until the database is open before you open the collection
+    if(dbOpen){
+        openCollection();
+    } else {
+        setTimeout(function(){
+            getCollection(name, callback);
+        }, 0);
+    }
 }
 
 /**
@@ -82,21 +82,21 @@ var getCollection = function(name, callback, veto){
  * - userObject will be null if no user exists
  ***/
 var findUser = function(username, callback){
-	getCollection(constants.DB_USERS, function(error, collection){
-		if(error){
-			callback(constants.ERR_INTERNAL, null);
-		} else {
-			// find a user with this username
-			var query = {username: username};
-			collection.findOne(query, function(error, result){
-				if(error){
-					callback(constants.ERR_INTERNAL, null);
-				} else {
-					callback(null, result);
-				}
-			});
-		}
-	});
+    getCollection(constants.DB_USERS, function(error, collection){
+        if(error){
+            callback(constants.ERR_INTERNAL, null);
+        } else {
+            // find a user with this username
+            var query = {username: username};
+            collection.findOne(query, function(error, result){
+                if(error){
+                    callback(constants.ERR_INTERNAL, null);
+                } else {
+                    callback(null, result);
+                }
+            });
+        }
+    });
 }
 
 /**
@@ -106,29 +106,29 @@ var findUser = function(username, callback){
  * - passwords are stored as PLAIN TEXT (do not do this IRL!)
  ***/
 var addUser = function(username, password, callback){
-	findUser(username, function(error, user){
-		if(error){
-			callback(error);
-		} else if(user !== null){
-			callback(constants.ERR_USER_EXISTS);
-		} else {
-			// add the user to the database
-			getCollection(constants.DB_USERS, function(error, collection){
-				if(error){
-					callback(constants.ERR_INTERNAL);
-				} else {
-					var user = {username: username, password: password};
-					collection.insert(user, function(error){
-						if(error){
-							callback(constants.ERR_INTERNAL);
-						} else {
-							callback(null);
-						}
-					});
-				}
-			});
-		}
-	});
+    findUser(username, function(error, user){
+        if(error){
+            callback(error);
+        } else if(user !== null){
+            callback(constants.ERR_USER_EXISTS);
+        } else {
+            // add the user to the database
+            getCollection(constants.DB_USERS, function(error, collection){
+                if(error){
+                    callback(constants.ERR_INTERNAL);
+                } else {
+                    var user = {username: username, password: password};
+                    collection.insert(user, function(error){
+                        if(error){
+                            callback(constants.ERR_INTERNAL);
+                        } else {
+                            callback(null);
+                        }
+                    });
+                }
+            });
+        }
+    });
 }
 
 /**
@@ -137,17 +137,17 @@ var addUser = function(username, password, callback){
  * - error will be null if operation successful
  ***/
 var deleteAllUsers = function(callback){
-	getCollection(constants.DB_USERS, function(error, collection){
-		if(error){
-			callback(constants.ERR_INTERNAL);
-		} else {
-			collection.drop(function(error){
-				if(error){
-					callback(constants.ERR_INTERNAL);
-				} else {
-					callback(null);
-				}
-			});
-		}
-	});
+    getCollection(constants.DB_USERS, function(error, collection){
+        if(error){
+            callback(constants.ERR_INTERNAL);
+        } else {
+            collection.drop(function(error){
+                if(error){
+                    callback(constants.ERR_INTERNAL);
+                } else {
+                    callback(null);
+                }
+            });
+        }
+    });
 }
