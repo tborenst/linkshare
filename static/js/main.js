08e2eaac847e37583e0f1283ee807ab0d8f7dbfd
Dillon Grove <dillongrove@gmail.com>
2014-03-06 23:54:34 -0500

    Handlebars, fake data, load function

--- static/js/main.js
+++ static/js/main.js
@@ -26,27 +26,33 @@ $(document).ready(function(){
         /* ..and add it back to the one that was just clicked */
         $(e.target).parent().addClass("tab_selected");
 
-        var nextPage = $(e.target.hash);
-        transition(nextPage, "crossfade");
+        var next_panel = $(e.target.hash);
+
+        /* fetch data for whatever page we're loading */
+        load_content(next_panel);
+
+        transition(next_panel, "crossfade");
     });
 
     /* bind all other panel links */
     $("#panels").on("click", "a.panel_link", function(e){
         e.preventDefault();
 
-        var nextPage = $(e.target.hash);
-        transition(nextPage, "push");
+        var next_panel = $(e.target.hash);
+        transition(next_panel, "push");
     });
 
     /* bind back button click event */
     $("#panels").on("click", ".back", function(e){
         /* find the last panel in history, and transition to it, if possible */
         e.preventDefault();
-        var lastPage = visits.back();
-        if(lastPage) {
-            transition(lastPage, "push", true);
+        var last_panel = visits.back();
+        if(last_panel) {
+            transition(last_panel, "push", true);
         }
     });
+
+    load_content($("#feed_panel"));
 
 });
 
@@ -67,7 +73,7 @@ function transition(toPanel, type, reverse) {
 
     toPanel
         .addClass("current " + type + " in " + reverse)
-        .one("webkitAnimationEnd", function(){
+        .one ("webkitAnimationEnd", function(){
             fromPanel.removeClass("current " + type + " out " + reverse);
             toPanel.removeClass(type + " in " + reverse);
         });
@@ -78,6 +84,27 @@ function transition(toPanel, type, reverse) {
         toPanel.addClass("current");
         fromPanel.removeClass("current");
         return;
+    }
+}
+
+/* a load function to simulate responses from a server */
+function load_content(next_panel){
+    var panel_to_load = next_panel.attr("id");
+    var load_target;
+    var content;
+
+    switch(panel_to_load){
+        case "feed_panel":
+            content = get_html("link_posts_template", LinkShare.posts);
+            console.log("content", content);
+            load_target = next_panel.find(".content_wrapper");
+            break;
+        default:
+            console.log("no data to load");
+    }
+
+    if(content){
+        $(load_target).html(content);
     }
 }
 
@@ -100,4 +127,41 @@ var visits = {
     clear: function() {
         this.history = [];
     }
+}
+
+/* ---------- HANDLEBARS NONSENSE ------------------------------------------- */
+
+/* Helper function to compile and render a handlebars template into html */
+function get_html(templateID, context){
+    var source = $("#" + templateID).html();
+    var template = Handlebars.compile(source);
+    var html = template(context);
+    return html;
+}
+
+/* ---------- DATA ---------------------------------------------------------- */
+
+var LinkShare = {
+    posts: [{
+        title: "Bouncer fights off gunman",
+        url: "i.imgur.com",
+        dateStr: "10:24 PM",
+        username: "tomer",
+        score: 4178
+    },
+    {
+        title: "GTA IV with mods... super photo-realistic",
+        url: "i.imgur.com",
+        dateStr: "10:17 PM",
+        username: "HuntingPandas",
+        score: 2186
+    },
+    {
+        title: "The cutest puppy came in my work last night.",
+        url: "i.imgur.com",
+        dateStr: "8:45 PM",
+        username: "t",
+        score: 278
+    },
+    ]
 }
